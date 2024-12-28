9d0797cb943f00be5762292b7dba598c070c4883
Данил Высоченко <vysochenkodanil@gmail.com>
2024-12-28 12:58:37 +0300

    Обновил модели, настройки и шаблоны, добавил миграции

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, IceCream, Topping, Wrapper
+
+admin.site.empty_value_display = 'Не задано'
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+
+class IceCreamAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )    
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+admin.site.register(IceCream, IceCreamAdmin)
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 1
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',        
+    )
+
+admin.site.register(Category, CategoryAdmin) 
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,35 +4,90 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
+    title = models.CharField(
+        max_length=256,
+        verbose_name= 'Название')
+    slug = models.SlugField(
+        max_length=64, unique=True,
+        verbose_name='Слаг')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения')
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+    def __str__(self):
+        return self.title
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    title = models.CharField(
+        max_length=256,
+        verbose_name= 'Название')
+    slug = models.SlugField(
+        max_length=64, unique=True,
+        verbose_name='Слаг')
+    class Meta:
+        verbose_name = 'топпинг'
+        verbose_name_plural = 'Топпинги'
+    def __str__(self):
+        return self.title
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
-
+    title = models.CharField(
+        max_length=256,
+        verbose_name= 'Название',
+        help_text='Уникальное название обёртки, не более 256 символов')
+    class Meta:
+        verbose_name = 'обертка'
+        verbose_name_plural = 'Обертки'
+    def __str__(self):
+        return self.title
+    
 
 class IceCream(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=256,
+        verbose_name= 'Название')
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
+
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
         related_name='ice_cream',
         null=True,
         blank=True,
+        verbose_name='обертка'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
+        verbose_name='Категория',
     )
-    toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
+    toppings = models.ManyToManyField(
+        Topping,
+        verbose_name ='топпинг'
+        )
+    is_on_main = models.BooleanField(
+        default=False,
+        verbose_name= 'На главную')
+    class Meta:
+        verbose_name = 'мороженное'
+        verbose_name_plural = 'Мороженное'
+        ordering = ('output_order', 'title')
+        
+    def __str__(self):
+        return self.title
 
